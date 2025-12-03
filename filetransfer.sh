3a947d2532a3b8b24ed4518edc45b27ee4a61fd5
Logan Luker <loganwluker@gmail.com>
2025-12-03 03:44:12 -0700

    Initial Commit. Burned haystack

--- filetransfer.sh
+++
@@ -1,5 +0,0 @@
-
-for HOST in wjcqmz@pc03-meb.emulab.net wjcqmz@pc07-fort.emulab.net wjcqmz@pc16-fort.emulab.net wjcqmz@pc12-fort.emulab.net wjcqmz@pc15-fort.emulab.net wjcqmz@pc06-fort.emulab.net wjcqmz@pc10-fort.emulab.net wjcqmz@pc13-fort.emulab.net wjcqmz@cnode-wasatch.emulab.net wjcqmz@cnode-moran.emulab.net
-do
-    scp QPSK_signal_2023_01_18_neal.iq $HOST:
-done
